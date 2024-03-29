386d9c016c0dec5cfe8499eda2278519b1ec3020
Michael Cao <caozibo2000@gmail.com>
2024-03-28 22:57:15 -0400

    1

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
 import { AppProvider } from "../../context/appContext";
-import { FunctionProvider } from "../../context/functionContext";
 
 export default function ThemeProvider({children} : {children: React.ReactNode}) {
 
   return (
     <AppProvider>
-      <FunctionProvider>
       <ChakraProvider theme={theme}>
           {children}
       </ChakraProvider>
-      </FunctionProvider>
     </AppProvider>
   )
 }
